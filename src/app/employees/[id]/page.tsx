52be981ff9eaaaad4fe563fdf1cd8b3ebf36311f
Samuel_Albuquerque <itzsamuel2003@gmail.com>
2026-09-03 09:28:35 -0400

    Adiciona ficha individual de materiais

---
+++ src/app/employees/[id]/page.tsx
@@ -0,0 +1,7 @@
+Get-Content : Não é possível localizar um parâmetro que coincida com o nome de parâmetro 'Raw'.
+No linha:2 caractere:13
++ Get-Content -Raw -Encoding UTF8 'src/app/employees/[id]/page.tsx'
++             ~~~~
+    + CategoryInfo          : InvalidArgument: (:) [Get-Content], ParameterBindingException
+    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.GetContentCommand
+ 
