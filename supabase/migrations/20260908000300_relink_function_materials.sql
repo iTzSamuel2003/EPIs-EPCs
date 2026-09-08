ca3d256010c5efb8f677ba625aa9e3c1db146203
Samuel_Albuquerque <itzsamuel2003@gmail.com>
2026-09-08 10:29:24 -0400

    fix: reatachar materiais ativos nas listas por funcao

---
+++ supabase/migrations/20260908000300_relink_function_materials.sql
@@ -0,0 +1,9 @@
+-- Reatacha listas por função aos materiais ativos após a consolidação do catálogo.
+update public.function_template_items set material_id = 'c2d8acae-7b3e-4abf-8029-ec91baa27ba4', material_name = 'Agulhão para colocação de linha de vida em poste de concreto' where material_id = '06bf1091-837f-4eaa-b9c5-87b7d9c2543d';
+update public.function_template_items set material_id = 'e8848972-8dfe-4a4e-a196-39c011109233', material_name = 'Arco de Serra +Lâmina' where material_id = '040d9bae-2fe4-4f76-8a42-c6be08bec10c';
+update public.function_template_items set material_id = '88486a03-30d6-4a39-ad8e-a795bb2c1855', material_name = 'Luva de cobertura' where material_id = '6c0c61ae-fe94-4e4e-b91f-9236ac93b0d7';
+update public.function_template_items set material_id = '593b7fc2-fefd-4e23-a6ed-a4182442a68e', material_name = 'Luva de vaqueta' where material_id = 'ab8e67fa-cc05-4047-a256-fbda2829cc89';
+update public.function_template_items set material_id = '718dea4a-76a3-405b-988f-1ba608eab95c', material_name = 'Luva isolante CL 0' where material_id = '3755dbc9-a636-47bc-96e4-21192ae8cbae';
+update public.function_template_items set material_id = '9a6b4e4c-8d6b-4676-9b6d-5e199797f4b3', material_name = 'Luva isolante CL II' where material_id = 'ddec552e-0be7-42bc-a732-7ab04fce6287';
+update public.function_template_items set material_id = 'e6a1dbb9-34a5-4243-befb-95d785306f1e', material_name = 'Manga isolante CL II' where material_id = 'a172d933-7ddb-4691-ace5-dcaea05b253f';
+update public.function_template_items set material_id = '3e2d6e46-9119-4c5f-90fe-148c36cba62e', material_name = 'Trava quedas' where material_id = '9953f749-8447-47e1-a70f-00a66ba34587';
